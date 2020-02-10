10a3aef9a3bce7ab8827f1c3f7636dcbe1651bab
Amir Ishanov <amirishanov@Amirs-MacBook-Pro.local>
2020-02-10 10:13:15 -0800

    Wireframe

--- css/main.js
+++ css/main.js
@@ -1,8 +1,7 @@
 const blackBoard = document.querySelectorAll('.black-board');
-const peg = document.querySelector('.whitepeg')
+const Peg = document.querySelector('.whitepeg');
 const players = {
     empty: 'black',
     black: 'pic1',
     white: 'pic2'
 }
-
