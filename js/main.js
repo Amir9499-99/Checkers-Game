6a40ce97c8e52c07c8e031571353ac6b8e7d120e
Amir Ishanov <amirishanov@Amirs-MBP.attlocal.net>
2020-02-11 23:30:56 -0800

    Add game logic

--- js/main.js
+++ js/main.js
@@ -12,27 +12,23 @@ const sqrEls = document.querySelectorAll('.board > div.black-board');
 const msgEl = document.querySelector('h3');
 
 //EventListeners
-document.querySelector('.board').addEventListener('click', handleBoardClick);
+sqrEls.forEach((square, idx) => square.addEventListener('click',(e) => handleBoardClick(e, idx)));
 
 //Variables
-let turn, winner, score, board, selectedPiece;
+let winner, score, board, selectedPiece;
+let turn = 0;
 
 function init(){
+    displayTurn()
     board = new Array(32).fill(null);
-    for (let i = 0; i < 12; i++) board[i] = new Piece(1);
-    for (let i = 20; i < 32; i++) board[i] = new Piece(-1);
-    turn =  1
-    console.log(board)
+    for (let i = 0; i < 12; i++) board[i] = new Piece(0);
+    for (let i = 20; i < 32; i++) board[i] = new Piece(1);
+    // console.log(board)
     winner = null;
     selectedPiece = null;
     render();
 }
 init()
-
-function flipTurn (){
-    turn *= -1;
-    console.log("Turn fliped")
-}
 
 function renderBoard(){
     sqrEls.forEach(function(el, idx){
@@ -41,9 +37,9 @@ function renderBoard(){
             cls = 'empty';
         } else if(board[idx] === selectedPiece){
             cls = 'selected-peg';
-        } else if(board[idx].player === 1){
+        } else if(board[idx].player === 0){
             cls = 'blue-peg';
-        } else if(board[idx].player === -1){
+        } else if(board[idx].player === 1){
             cls = 'gold-peg';
         } 
         el.innerHTML = `<div class="${cls}"></div>`;
@@ -53,9 +49,39 @@ function renderBoard(){
 
 function render(){
     renderBoard();
-    msgEl.textContent = `It's ${turn === 1 ? "Blue's" : "Gold's"} turn`
+
+    
 }
 
-function handleBoardClick(evt){
-    
+function handleBoardClick(evt, idx){
+
+      console.log(evt.target.className)
+
+    if (evt.target.className !== 'black-board'){
+        if (evt.target.className === 'blue-peg' && turn%2 === 0){
+            evt.target.classList.remove('blue-peg');   
+            evt.target.classList.add('empty');   
+        }else if(evt.target.className === 'gold-peg' && turn%2 === 1){
+            evt.target.classList.remove('gold-peg')
+            evt.target.classList.add('selected-peg-gold')
+        }else if(evt.target.className === 'empty' && turn%2 === 0){
+            evt.target.classList.remove('empty')
+            evt.target.classList.add('blue-peg');
+            flipTurn()
+            displayTurn()
+        }else{
+            evt.target.classList.remove('empty')
+            evt.target.classList.add('gold-peg');
+            flipTurn()
+            displayTurn()
+        }   
+    }
+}
+
+function flipTurn(){
+    turn++;
+}
+
+function displayTurn(){
+    msgEl.textContent = `It's ${(turn % 2 === 0) ? "Blue's" : "Gold's"} turn`
 }
